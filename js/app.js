12f76fd9d0f6343ea97283bc6ae90915d665668c
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-07-04 18:45:44 +0900

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// 所持金の入力
+let budget = prompt('所持金を数字で入力してください');
+budget = parseFloat(budget);
+// 所持金で条件分岐する
+if (budget >= 1500) {
+alert('ピザを注文しました');
+//所持金が1500円未満の処理
+} else if (budget >= 500) {
+  alert('ポテトを注文しました');
+} else {
+  alert('節約、節約...');
+}
