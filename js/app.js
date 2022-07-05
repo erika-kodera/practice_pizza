a680a547007efd7233dbd2a018a84a7ce9ac07b4
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-07-05 11:14:00 +0900

    pizza注文プログラム
    ・所持金が入力でき、所持金に応じて条件が分岐する様に記載した。
    ・お腹の空き具合に応じて条件が分岐する様にした。

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // 所持金の入力
 let budget = prompt('所持金を数字で入力してください');
 budget = parseFloat(budget);
+// お腹の空き具合に応じて条件分岐
+let isHungry = confirm('お腹は空いていますか？');
 // 所持金で条件分岐する
-if (budget >= 1500) {
+if (budget >= 1500 && isHungry) {
 alert('ピザを注文しました');
 //所持金が1500円未満の処理
 } else if (budget >= 500) {
